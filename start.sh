9a5d90159da5ae0e350a85fc6179390856f5819f
mr <mihkel@jaculus.eu>
2018-03-27 12:46:30 +0300

    Keys changed App install from git

--- start.sh
+++ start.sh
@@ -3,21 +3,19 @@
 
 # path
 P=`dirname "$0"`
-echo $P
 cd $P
-echo `pwd`
 
 # do we have aws installed ?
 if ! [ -x "$(command -v aws)" ]; then
-	echo 'Error: aws is not installed.' >&2
+	echo 'Error: aws is not installed ...' >&2
 	exit 1
 fi
 
 # do we have ssh key ?
 if [ ! -f aws/hellokeys.pem ]; then
-    echo "AWS ssh key not found, create"
+    echo "AWS ssh key not found, creating one ..."
 	aws ec2 delete-key-pair --key-name hellokeys
 	aws ec2 create-key-pair --key-name hellokeys --query 'KeyMaterial' --output text > aws/hellokeys.pem
 fi
 
-#aws cloudformation create-stack --stack-name hello-world-stack  --template-body file://$PWD/aws/hello-world-aws.json
+aws cloudformation create-stack --stack-name hello-world-stack  --template-body file://$PWD/aws/hello-world-aws.json
