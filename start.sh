1c42f8c6f23028c19b3d98b2a13eb2e7c4e188e6
mr <mihkel@jaculus.eu>
2018-03-27 12:51:45 +0300

    Shell script modifications

--- start.sh
+++ start.sh
@@ -11,6 +11,16 @@ if ! [ -x "$(command -v aws)" ]; then
 	exit 1
 fi
 
+# set region
+aws configure set region us-west-2
+
+# configure aws
+echo "Current aws configuration"
+aws configure list
+
+echo "Lets go over with aws config"
+aws configure
+
 # do we have ssh key ?
 if [ ! -f aws/hellokeys.pem ]; then
     echo "AWS ssh key not found, creating one ..."
